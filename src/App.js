89a3de250d5eaae27a34f3e530bdc783cd3e5289
Penina Wanjiru <penina.wanjiru@andela.com>
2020-01-28 20:42:55 +0300

    Change import path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import HomePage from './pages/components/homepage.component';
+import HomePage from './pages/components/homepage/homepage.component';
 
 function App() {
   return (
