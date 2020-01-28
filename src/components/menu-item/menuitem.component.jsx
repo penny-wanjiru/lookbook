add19bd9f0863505d0a16145c39ef4a7a71e077d
Penina Wanjiru <penina.wanjiru@andela.com>
2020-01-28 21:17:37 +0300

    Create directory component

--- src/components/menu-item/menuitem.component.jsx
+++ src/components/menu-item/menuitem.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 
-const MenuItem = () => (
+const MenuItem = ({title}) => (
 	<div className = "menu-item">
 		<div className = "content">
-			<h1 className="title">HATS</h1>
+			<h1 className="title">{title}</h1>
 			<span className="subtitle">SHOP NOW</span>
 		</div>
 	</div>
 );
+
+export default MenuItem;
