d3ee89f8c801ee5c2967547c3a869f501eadf294
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-04 16:40:28 +0300

    Add switch component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/components/homepage/homepage.component';
 
 function App() {
   return (
     <div className="App">
+		<Switch>
 			<Route exact path='/' component={ HomePage }/>
+		</Switch>
     </div>
   );
 }
