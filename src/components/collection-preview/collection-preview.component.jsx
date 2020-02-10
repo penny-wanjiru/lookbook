6516723deeaed2aa1f4a95486d6402d63238b826
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-10 22:28:05 +0300

    Map through collection items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const collectionPreview({title, items}) => (
-		<div>
-			<h1>{title}</h1>
+		<div className='collection-preview'>
+			<h1 className='title'>{title.toUpperCase()}</h1>
 			<div className="preview">
-				{items.map(item=>(<div>{items.name}</div>)}
+				{items.map(item=>(<div key={items.name}>{items.name}</div>)}
 			</div>
 		</div>
 )
