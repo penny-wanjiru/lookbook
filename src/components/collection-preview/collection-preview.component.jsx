c50af19c1cb70e1b200a51d9608e4cfe40246fcc
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-11 15:46:35 +0300

    Changed to named export

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const CollectionPreview = ({title, items}) => (
+export const CollectionPreview = ({title, items}) => (
 		<div className='collection-preview'>
 			<h1 className='title'>{title.toUpperCase()}</h1>
 			<div className="preview">
-				{items.map(item=>(
-					<div key={items.name}>{items.name}</div>
+				{items.map((item)=>(
+					<div key={item.id}>{item.name}</div>
 				))}
 			</div>
 		</div>
 );
-
-
-export default CollectionPreview;
