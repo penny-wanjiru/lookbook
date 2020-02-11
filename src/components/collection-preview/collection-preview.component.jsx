e0f8e43569e3a3ad821867f2bfcceba06ad7c36e
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-11 17:20:03 +0300

    Refactor imports

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './collection-preview.styles.scss';
 import CollectionItem from './../collection-item/collection-item.component'
 
-export const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({title, items}) => (
 		<div className='collection-preview'>
 			<h1 className='title'>{title.toUpperCase()}</h1>
 			<div className="preview">
@@ -13,3 +13,5 @@ export const CollectionPreview = ({title, items}) => (
 			</div>
 		</div>
 );
+
+export default CollectionPreview;
