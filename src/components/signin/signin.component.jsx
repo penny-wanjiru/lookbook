e85f06259287ae9b6f25a8dccec27ab91909d0ee
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-12 14:35:37 +0300

    Add handle input change method

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -17,15 +17,20 @@ class SignIn extends React.Component {
 		this.setState({name:'',email:''})
 	);
 
+	handleChange = event => (
+		const {name, value} = event.target;
+		this.setState({[name]:value});
+	)
+
 	render(){
 		return {
 			<div className='signin'>
 				<h1>I already have an account</h1>
 				<span>signin with your email and password</span>
 				<form onSubmit={this.handleSubmit}>
-					<input name='email' type='email' value={this.state.email} required/>
+					<input onChange={this.handleChange} name='email' type='email' value={this.state.email} required/>
 					<label>Email</label>
-					<input name='password' type='password' value={this.state.password} required/>
+					<input onChange={this.handleChange} name='password' type='password' value={this.state.password} required/>
 					<label>Password</label>
 					<input type='submit' value='Submit Form'/>
 				</form>
