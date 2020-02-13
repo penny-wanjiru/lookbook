c8f92a815a662adca9ab73ca7877b3e33346e09a
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-13 19:43:11 +0300

    Add children to button component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({children, ...otherProps}) => (
-	<button className='custom-button' {...otherProps}/>
+	<button className='custom-button' {...otherProps}>
+	{children}
+	</button>
 )
 
 export default CustomButton;
