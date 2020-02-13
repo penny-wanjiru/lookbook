f1ee66fa23408cb1ce1f316de32129ed2713cd69
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-13 17:01:50 +0300

    Add signin labels

--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './form.styles.scss'
+
 const Form =({handleChange, label, ...otherFormProps})=> (
 	<div className='group'>
 		<input className='form-input' onChange={handleChange} {...otherFormProps} />
 		{
 			label?(
 			<label className={`${otherFormProps.value.length?'shrink':''} form-input-label`}>
+			{label}
 			</label>
 			)
 			:null
