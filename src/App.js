6d1a3fa759f43ca2ebb2ab7fd40a2203a8bd33f5
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-16 13:38:21 +0300

    Add sign out

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
 	componentDidMount(){
 		this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
 		this.setState({currentUser:user});
+		console.log(user);
 	})
 	}
 
@@ -32,7 +33,7 @@ class App extends React.Component {
 	render (){
 	  return (
 	    <div className="App">
-				<Header/>
+				<Header currentUser={this.state.currentUser}/>
 				<Switch>
 					<Route exact path='/' component={ HomePage }/>
 					<Route exact path='/shop' component={ ShopPage }/>
