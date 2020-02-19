7def726857d9a80102c8cc0d9e87337eb2786216
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-19 15:43:35 +0300

    Finish signin handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ class App extends React.Component {
 							 id:snapShot.id,
 							 ...snapShot.data()
 						 }
-					 })
+					 });
+					 // console.log(this.state);
 				 });
 			}
 			this.setState({ currentUser:userAuth});
--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './signin.styles.scss';
 import Form from '../form/form.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -15,10 +15,18 @@ class SignIn extends React.Component {
 		}
 	}
 
-	handleSubmit = event => (
-		event.preventDefault(),
-		this.setState({name:'',email:''})
-	);
+	handleSubmit = async event => {
+		event.preventDefault();
+		const {email, password} = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({email:'', password:''});
+		}
+		catch(error){
+			console.log(error);
+		}
+		// this.setState({name:'',email:''})
+	};
 
 	handleChange = event => {
 		const {name, value} = event.target;
