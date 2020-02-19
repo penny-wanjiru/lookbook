5607f8f4cb3ed21610a803e19ca42c477ed00d64
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-19 13:20:42 +0300

    save user details in firestore

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/components/homepage/homepage.component';
 import ShopPage from './pages/components/shop/shop.component';
 import SigninAndSignup from './pages/components/signin-and-signup/signin-and-signup.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
 
@@ -20,9 +20,10 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount(){
-		this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-		this.setState({currentUser:user});
-		console.log(user);
+		this.unsubscribeFromAuth=auth.onAuthStateChanged(async user=>{
+		// this.setState({currentUser:user});
+		createUserProfileDocument(user);
+		// console.log(user);
 	})
 	}
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,6 +16,29 @@ const firebaseConfig = {
     appId: "1:809870872678:web:00fa902a81ea63f303dda7"
   };
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if(!userAuth) return;
+	const userRef = store.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	if(!snapShot.exists){
+		const { displayName, email} = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			})
+		}catch(error){
+			 console.log('error creating user', error.message);
+		}
+	}
+	return userRef;
+}
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
