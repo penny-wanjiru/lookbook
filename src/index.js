869f6b08e4a7f88dd67010c1f5b82e803dfde1cd
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-20 12:10:11 +0300

    Set up store and pass into provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-	<Provider>
+	<Provider store={store}>
 		<BrowserRouter>
 			<App/>
 		</BrowserRouter>
