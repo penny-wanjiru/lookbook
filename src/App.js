49537a0a9004a7d131978dd04503549280ad3944
Penina Wanjiru <penina.wanjiru@andela.com>
2020-02-22 18:25:40 +0300

    Minor refactors

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ShopPage from './pages/components/shop/shop.component';
 import SigninAndSignup from './pages/components/signin-and-signup/signin-and-signup.component';
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-import {setCurrentUser} from './redux/user/user.actions ';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
 
@@ -18,20 +18,19 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount(){
+		const { setCurrentUser } = this.props;
 		this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
 			if (userAuth){
 				const userRef = await createUserProfileDocument(userAuth);
 				 userRef.onSnapshot(snapShot => {
-					 this.setState({
-						 currentUser:{
+					 setCurrentUser({
 							 id:snapShot.id,
 							 ...snapShot.data()
-						 }
 					 });
 					 // console.log(this.state);
 				 });
 			}
-			this.setState({ currentUser:userAuth});
+			setCurrentUser({userAuth});
 	})
 	}
 
@@ -54,7 +53,7 @@ class App extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-	setCurrentUser: user => dispatch(user)
+	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(null, mapDispatchToProps)(App);
